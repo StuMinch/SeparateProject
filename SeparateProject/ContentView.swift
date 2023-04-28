6d77f8bedbe622debb72b3dd7daa5eb517c22442
Stuart <stumin@saucelabs.com>
2023-04-28 14:47:53 -0700

    Initial Commit

---
+++ SeparateProject/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SeparateProject
+//
+//  Created by Stuart Minchington on 4/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SeparateProject/SeparateProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeparateProjectApp.swift
+//  SeparateProject
+//
+//  Created by Stuart Minchington on 4/28/23.
+//
+
+import SwiftUI
+
+@main
+struct SeparateProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
