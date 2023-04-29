c7748e5263fc2459fed642f088afe3b8b064b6c2
Stuart <stumin@saucelabs.com>
2023-04-28 19:46:14 -0700

    Created a separate Xcode project specifically for my XCUITests that I can run against an app made in a completely different project.

--- SeparateProjectUITests/SeparateProjectUITests.swift
+++ SeparateProjectUITests/SeparateProjectUITests.swift
@@ -8,34 +8,21 @@
 import XCTest
 
 final class SeparateProjectUITests: XCTestCase {
+    var app: XCUIApplication!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+      try super.setUpWithError()
+      continueAfterFailure = false
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+      app = XCUIApplication(bundleIdentifier: "com.example.apple-samplecode.Scrumdinger")
+      app.launch()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    func testAddingScrumWithDefaultValues() throws {
+        app.navigationBars["Daily Scrums"]/*@START_MENU_TOKEN@*/.buttons["New Scrum"]/*[[".otherElements[\"New Scrum\"].buttons[\"New Scrum\"]",".buttons[\"New Scrum\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.collectionViews/*@START_MENU_TOKEN@*/.textFields["Title"]/*[[".cells.textFields[\"Title\"]",".textFields[\"Title\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.collectionViews.textFields["Title"].typeText("UI Test Scrum")
+        app.buttons["Add"].tap()
+        XCTAssertTrue(app.staticTexts["UI Test Scrum"].exists)
     }
 }
